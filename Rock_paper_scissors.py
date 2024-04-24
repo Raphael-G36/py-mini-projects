7b97b321e786fc1cbff4e5a8b8596ab804b2562d
raphael <raphaelokonmah3@gmail.com>
2024-04-24 10:38:08 +0100

    edited rock paper scissors game

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -5,27 +5,31 @@ answer = input ('Do you wish to play (y/n)? ')
 if answer.lower() == 'y':
     while True:
         choices = ['Rock', 'Paper', 'Scissors']
-        val = random.randint(2)
+        val = random.randint(0,2)
         compChoice = choices[val]
 
         ans = input ('Which do you chose.. Rock,Paper,scissors or q to quit? ')
         if ans.lower() == 'q':
+            print ('Thanks for playing')
             break
+        elif ans.lower() not in ['rock','paper','scissors']:
+            print ('Please enter a valid choice')
+            continue
         else:
             if ans.lower() == compChoice.lower():
-                print ("oops! you both made the same choices, Try Again")
+                print ("Computer chose" ,compChoice, "oops! you both made the same choices, Try Again")
                 continue
             else:
                 if ans.lower() == 'rock' and  compChoice == 'Scissors':
-                    print("you win!")
+                    print("Computer chose" ,compChoice, "you win!")
                     continue
                 elif ans.lower() == 'paper' and  compChoice == 'Rock':
-                    print("you win!")
+                    print("Computer chose" ,compChoice,"you win!")
                     continue
                 elif ans.lower() =='scissors' and  compChoice == 'Paper':
-                    print("you win!")
+                    print("Computer chose" ,compChoice, "you win!")
                     continue
                 else:
-                    print("you lose!")
+                    print("Computer chose" ,compChoice, "you lose!")
                     continue
                 
